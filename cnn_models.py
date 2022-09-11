15b8752762939002de7997dd2832f4fcf2ede851
rizvi23061998 <rizvi23061998@gmail.com>
2022-09-11 16:33:05 +0600

    Modified Dimensions again

--- cnn_models.py
+++ cnn_models.py
@@ -14,18 +14,20 @@ class CNN2Layers(torch.nn.Module):
             torch.nn.Conv1d(in_channels=feature_channels, out_channels=int(feature_channels/2), kernel_size=kernel_size,
                             stride=stride, padding=padding),
             torch.nn.ELU(),
-            torch.nn.Dropout(dropout)
+            torch.nn.Dropout(dropout),
+            torch.nn.Conv1d(in_channels = int(feature_channels/2), out_channels=1, kernel=31,
+                            stride = 1, padding= 0 )
         )
         
-        self.fc = torch.nn.Linear(int(feature_channels/2)*31*batch_size, batch_size)
+        # self.fc = torch.nn.Linear(int(feature_channels/2)*31*batch_size, batch_size)
             
 
     def forward(self, x):
         x = self.conv1(x)
-        print(x.shape)
-        x = torch.flatten(x)
-        print(x.shape)
-        x = self.fc(x)
-        return x
-        # return torch.squeeze(x)
+        # print(x.shape)
+        # x = torch.flatten(x)
+        # print(x.shape)
+        # x = self.fc(x)
+        # return x
+        return torch.squeeze(x)
 
